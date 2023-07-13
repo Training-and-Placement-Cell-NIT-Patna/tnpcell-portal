00a323199c1832cd25215704853f1bcd9a44a331
krcpr007@gmail.com <rajdeepnagar08@gmail.com>
2023-07-13 14:24:11 +0530

    meta description added

--- frontend/pages/loginPage.js
+++ frontend/pages/loginPage.js
@@ -1,12 +1,17 @@
-import SignIn from '@/components/SignIn'
-import React from 'react'
-import { ToastContainer } from 'react-toastify'
-
+import SignIn from "@/components/SignIn";
+import React from "react";
+import { ToastContainer } from "react-toastify";
+import Head from "next/head";
 export default function loginPage() {
   return (
-    <div>
+    <>
+      <Head>
+        <title>Login | NIT Patna</title>
+        <meta name="viewport" content="initial-scale=1.0, width = device-width" />
+        <meta name="description" content="Placement portal of nit patna | NIT Patna Training and Placement Portal" />
+      </Head>
       <ToastContainer
-        position='top-right'
+        position="top-right"
         autoClose={5000}
         hideProgressBar={false}
         newestOnTop={false}
@@ -17,6 +22,6 @@ export default function loginPage() {
         pauseOnHover
       />
       <SignIn />
-    </div>
-  )
+    </>
+  );
 }
