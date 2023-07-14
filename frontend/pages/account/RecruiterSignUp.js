f866e4557ef495a876f02fbecb1c5b3b6ca36a1e
krcpr007@gmail.com <rajdeepnagar08@gmail.com>
2023-07-14 15:46:24 +0530

    style : btn color changed

--- frontend/pages/account/RecruiterSignUp.js
+++ frontend/pages/account/RecruiterSignUp.js
@@ -56,7 +56,7 @@ export default function SignUpRecuiter({ token = ''}) {
           return
         }
          
-        console.log("xy")
+        // console.log("xy")
         const profile = await res.json()
         // console.log(JSON.stringify(profile, null, 2))
         toast.error(profile?.error.name)
@@ -64,7 +64,7 @@ export default function SignUpRecuiter({ token = ''}) {
         
         toast.success('Registered ! We Contact You Soon')
 
-        console.log("yz")
+        // console.log("yz")
 
         // const profile = await res.json()
         router.push(`/loginPage`)
@@ -100,7 +100,7 @@ const handleInputChange = (e) => {
           <p className='mt-2 text-center text-sm text-gray-600'>
           Let&apos;s get you registered Or{' '}
             <Link href='/'>
-              <a className='font-medium text-indigo-600 hover:text-indigo-500'>
+              <a className='font-medium text-yellow-600 hover:text-yellow-500'>
                 Login
               </a>
             </Link>
@@ -160,9 +160,6 @@ const handleInputChange = (e) => {
                   />
                 </div>
               </div>
-
-
-
               <div>
                 <label
                   htmlFor='contact_no'
@@ -184,9 +181,6 @@ const handleInputChange = (e) => {
                   />
                 </div>
               </div>
-
-
-
               <div>
                 <label
                   htmlFor='email'
@@ -213,7 +207,7 @@ const handleInputChange = (e) => {
               <div>
                 <button
                   type='submit'
-                  className='w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+                  className='w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-yellow-600 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500'
                 >
                   Register
                 </button>
