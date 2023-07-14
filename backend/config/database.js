bc84481c3aea9386bcbe98e5b7e2a149583e0d5b
krcpr007@gmail.com <rajdeepnagar08@gmail.com>
2023-07-14 14:17:30 +0530

    postgresql added

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,38 +1,33 @@
-const path = require('path');
+// const path = require('path');
 
-/*
-// Copied from https://docs.strapi.io/developer-docs/latest/setup-deployment-guides/configurations/required/databases.html#configuration-structure
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'sqlite',
+//     connection: {
+//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+//     },
+//     useNullAsDefault: true,
+//   },
+// });
+
 module.exports = ({ env }) => ({
-  connection: {
-    client: 'mysql',
-    connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'strapi'),
-      password: env('DATABASE_PASSWORD', 'IITP@strapi123'),
-      ssl: {
-        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+      connection: {
+        client: 'postgres',
+        connection: {
+          host: env('DATABASE_HOST', 'localhost'),
+          port: env.int('DATABASE_PORT', 5432),
+          database: env('DATABASE_NAME', 'tnp-test'),
+          user: env('DATABASE_USERNAME', 'postgres'),
+          password: env('DATABASE_PASSWORD', '2006001'),
+          schema: env('DATABASE_SCHEMA', 'public'), // Not required
+          // ssl: {
+          //   rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+          // },
+        },
+        debug: false,
       },
-    },
-    debug: false,
-  },
-});
-*/
-
-/* Previous */
-
-
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
-    connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-    },
-    useNullAsDefault: true,
-  },
-});
-
+    });
 
 /*
 
