0b5b5570debc15b5b48c015b6516cb208ce5b8d9
Tnp main computer <tech.tnp@nitp.ac.in>
2023-07-26 12:44:01 +0530

    UI : Companies marquee added

---
+++ frontend/components/LandingPageElements/CompaniesMarquee.js
@@ -0,0 +1,68 @@
+import React from 'react'
+
+function CompaniesMarquee() {
+    return (
+        <>
+            <div>
+                <h1 className='text-center font-medium text-red-900 text-xl lg:text-4xl'>NIT Patna Student's Gets Placed In</h1>
+            </div>
+            <section class="container mb-16">
+                <section class="wrapper">
+                    <section class="firstScroll">
+                        <section class="first">
+                            <img src="https://cdn.worldvectorlogo.com/logos/amazon-2.svg" alt="first" height="120px" width="120px" />
+                        </section>
+                        <section class="second">
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/b/bd/Indian_Space_Research_Organisation_Logo.svg" alt="first" height="120px" width="120px" />
+                        </section>
+                        <section class="third">
+                            <img src="https://cdn.worldvectorlogo.com/logos/atlassian-1.svg" alt="first" height="120px" width="120px" />
+                        </section>
+                        <section class="fourth">
+                            <img src="https://cdn.worldvectorlogo.com/logos/phonepe-1.svg" alt="first" height="120px" width="120px" />
+                        </section>
+                        <section class="fifth">
+                            <img src="https://images.crunchbase.com/image/upload/c_lpad,h_170,w_170,f_auto,b_white,q_auto:eco,dpr_1/v1456398461/tlbgy0u13v8upv9n8mdm.png" alt="first" height="120px" width="120px" />
+                        </section>
+                        <section class="sixth">
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/b/ba/TexasInstruments-Logo.svg" alt="first" height="120px" width="120px" />
+                        </section>
+                        <section class="seventh">
+                            <img src="https://cdn.worldvectorlogo.com/logos/adobe-2.svg" alt="first" height="120px" width="120px" />
+                        </section>
+
+                    </section>
+                    <section class="firstScroll">
+                        <section class="first">
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Byju%27s_logo.svg/1920px-Byju%27s_logo.svg.png" alt="first" height="120px" width="120px" />
+                        </section>
+                        <section class="second">
+                            <img src="https://cdn.worldvectorlogo.com/logos/samsung.svg" alt="first" height="120px" width="120px" />
+                        </section>
+                        <section class="third">
+                            <img src="https://cdn.worldvectorlogo.com/logos/google-1-1.svg" alt="first" height="120px" width="120px" />
+                        </section>
+                        <section class="fourth">
+                            <img src="https://cdn.worldvectorlogo.com/logos/apple-11.svg" alt="first" height="120px" width="120px" />
+                        </section>
+                        <section class="fifth">
+                            <img src="https://cdn.worldvectorlogo.com/logos/morgan-stanley-1.svg" alt="first" height="120px" width="120px" />
+                        </section>
+                        <section class="sixth">
+                            <img src="https://cdn.worldvectorlogo.com/logos/meta-1.svg" alt="first" height="120px" width="120px" />
+                        </section>
+                        <section class="seventh">
+                            <img src="https://cdn.worldvectorlogo.com/logos/paytm-1.svg" alt="first" height="120px" width="120px" />
+                        </section>
+
+                    </section>
+
+                </section>
+
+            </section>
+
+        </>
+    )
+}
+
+export default CompaniesMarquee
