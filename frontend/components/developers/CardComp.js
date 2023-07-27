a46938aea640b6f098cd25227b1c26530480400a
Tnp main computer <tech.tnp@nitp.ac.in>
2023-07-27 18:37:58 +0530

    created : developers page and comp

---
+++ frontend/components/developers/CardComp.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { BsTwitter, BsLinkedin, BsGithub } from "react-icons/bs";
+function CardComp({ data }) {
+  return (
+    <>
+      <div>
+        <div className="relative overflow-hidden transition duration-300 transform rounded shadow-lg lg:hover:-translate-y-2 hover:shadow-2xl">
+          <img
+            className="object-cover w-full h-56 md:h-64 xl:h-80"
+            src={data.image}
+            alt="Person"
+          />
+          <div className="absolute inset-0 flex flex-col justify-center px-5 py-4 text-center transition-opacity duration-300 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
+            <p className="mb-1 text-lg font-bold text-gray-100">{data.name}</p>
+            <p className="mb-4 text-xs text-gray-100">{data.role}</p>
+            <p className="mb-4 text-xs tracking-wide text-gray-400">
+              {data.about}
+            </p>
+            <div className="flex items-center justify-center space-x-3">
+              <a
+                href={data.twitter}
+                className="text-white transition-colors duration-300 hover:text-teal-accent-400"
+              >
+                <BsTwitter />
+              </a>
+              <a
+                href={data.linkedin}
+                className="text-white transition-colors duration-300 hover:text-teal-accent-400"
+              >
+                <BsLinkedin />
+              </a>
+              {/* <a
+                href={data.github}
+                className="text-white transition-colors duration-300 hover:text-teal-accent-400"
+              >
+                <BsGithub />
+              </a> */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default CardComp;
