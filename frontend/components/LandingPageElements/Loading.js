f1ee5c0d8398fb8ae69a98db11a8891aef7e2330
Tnp main computer <tech.tnp@nitp.ac.in>
2023-07-27 17:41:53 +0530

    loader changed

--- frontend/components/LandingPageElements/Loading.js
+++ frontend/components/LandingPageElements/Loading.js
@@ -1,17 +1,18 @@
-import React from 'react'
-import Image from 'next/image'
-import { CogIcon } from '@heroicons/react/solid'
+import React from "react";
+// import Image from "next/image";
+// import { CogIcon } from "@heroicons/react/solid";
+import Lottie from "lottie-react";
+import loadingAnimation from "public/lottie/loadingAnimation";
 function Loading() {
   return (
-    <div className='font-bold text-4xl flex items-center relative  text-red-900 bg-white min-h-screen'>
-      <div className='absolute top-1/3 left-1/2 grid grid-rows-2 justify-items-center'>
-        
-        <CogIcon className='motion-safe:animate-spin text-yellow-500 h-20 w-20'/>
-        
-        Loading
-      </div>
+      <div className="flex justify-center mt-28 md:grid md:grid-cols-1 md:justify-items-center md:mt-0 bg-white">
+        <Lottie
+          animationData={loadingAnimation}
+          className="flex justify-center items-center w-1/2"
+          loop={true}
+        />
     </div>
-  )
+  );
 }
 
-export default Loading
+export default Loading;
