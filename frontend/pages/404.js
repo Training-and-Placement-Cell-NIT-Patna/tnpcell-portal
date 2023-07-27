9ac8af8991d5c4c6e4b20be2b6ebb142cd434da0
Tnp main computer <tech.tnp@nitp.ac.in>
2023-07-27 14:21:01 +0530

    code refactor

--- frontend/pages/404.js
+++ frontend/pages/404.js
@@ -1,41 +1,41 @@
-import Image from 'next/image'
-import Link from 'next/link'
+import Image from "next/image";
+import Link from "next/link";
 
 export default function NotFound() {
   return (
     <>
-      <div className='min-h-full pt-16 pb-12 flex flex-col bg-white'>
-        <main className='flex-grow flex flex-col justify-center text-4xl pt-9  max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8'>
-          
-          <div className='py-16'>
-            <div className='text-center'>
-              <p className='font-extrabold text-yellow-500 uppercase tracking-wide '>
-                <div className=''>
-            <Link href='/'>
-              <span className='text-9xl '>
-                4
-                <Image
-                  className='mx-auto animate-spin'
-                  width={100}
-                  height={100}
-                  alt='NIT Patna'
-                  src='/images/logo.png'
-                />
-                4
-              </span>
-            </Link>
-          </div> error
+      <div className="min-h-full pt-16 pb-12 flex flex-col bg-white">
+        <main className="flex-grow flex flex-col justify-center text-4xl pt-9  max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="py-16">
+            <div className="text-center">
+              <p className="font-extrabold text-yellow-500 uppercase tracking-wide ">
+                <div className="">
+                  <Link href="/">
+                    <span className="text-9xl ">
+                      4
+                      <Image
+                        className="mx-auto animate-pulse"
+                        width={100}
+                        height={100}
+                        alt="NIT Patna"
+                        src="/images/logo.png"
+                      />
+                      4
+                    </span>
+                  </Link>
+                </div>{" "}
+                error
               </p>
-              <h1 className='mt-2 text-5xl font-extrabold text-gray-900 tracking-tight sm:text-5xl'>
+              <h1 className="mt-2 text-5xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">
                 Page not found.
               </h1>
-              <p className='mt-2 text-base text-gray-600'>
+              <p className="mt-2 text-base text-gray-600">
                 Sorry!! we couldn’t find the page you’re looking for.
               </p>
-              <div className='mt-6'>
-                <Link href='/' className=''>
-                  <button className='transition text-slate-200 text-sm rounded-lg ease-in-out p-3 delay-150 bg-yellow-500 hover:-translate-y-1 hover:scale-110 hover:bg-orange-500 duration-300'>
-                    Go back home<span aria-hidden='true'> &rarr;</span>
+              <div className="mt-6">
+                <Link href="/" className="">
+                  <button className="transition text-slate-200 text-sm rounded-lg ease-in-out p-3 delay-150 bg-yellow-500 hover:-translate-y-1 hover:scale-110 hover:bg-orange-500 duration-300">
+                    Go back home<span aria-hidden="true"> &rarr;</span>
                   </button>
                 </Link>
               </div>
@@ -44,5 +44,5 @@ export default function NotFound() {
         </main>
       </div>
     </>
-  )
+  );
 }
