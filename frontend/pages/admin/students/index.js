587a071f0e128a90d1596bdabcbf7bd2a7293524
Sahitya Aryan <sahityaaryansingh@gmail.com>
2023-08-19 12:00:05 +0530

    Fix : Set the width and the height in Ag-grid-react component

--- frontend/pages/admin/students/index.js
+++ frontend/pages/admin/students/index.js
@@ -621,7 +621,7 @@ export default function Students({ token }) {
           </div>
         </div>
       </div>
-      <div className='ag-theme-alpine mt-4' style={{ height: 'auto' }}>
+      <div className='ag-theme-alpine mt-4' style={{ margin: "auto", height: "400px", width: "1100px" }}>
         <AgGridReact
           ref={gridRef}
           rowMultiSelectWithClick={true}
