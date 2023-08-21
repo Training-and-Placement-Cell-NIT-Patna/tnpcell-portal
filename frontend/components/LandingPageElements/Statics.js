ea66bd66324112a800727a95b555c52a0086e4d1
Sahitya Aryan <sahityaaryansingh@gmail.com>
2023-08-21 09:38:50 +0530

    Added : the libraries and required components to show the barChart

--- frontend/components/LandingPageElements/Statics.js
+++ frontend/components/LandingPageElements/Statics.js
@@ -1,3 +1,11 @@
+
+
+// import { Bar } from "react-chartjs-2";
+import BarChart from "../Charts/BarChart";
+import Chart from "chart.js/auto";
+import { CategoryScale } from "chart.js";
+// import
+// import Image from "next/image";
 // import React from 'react'
 // import {
 //     Chart as ChartJS,
@@ -20,6 +28,8 @@
   //     Tooltip,
   //     Legend
   //   )
+
+Chart.register(CategoryScale);
   
 
 function Statics() {
@@ -39,10 +49,135 @@ function Statics() {
         'December'
     ];
     //   const labels = MONTHS({count: 7});
+
+    //data for BTech seat Matrix
+
+    const BtechSeatMatrixData = {
+
+        labels:["B Arch", "Civil Enginn." , "Computer Sci. Engin.","Electronics & comm. Enginne." , "Electrical Enginne." , "Mechanical Engin." , "Integrated Msc"],
+
+       datasets:[
+        {
+               labels: "",
+
+               data: [ 40,101, 157, 127, 100, 105, 50],
+
+               backgroundColor: [
+                   'rgba(3, 38, 224)',
+                   'rgba(3, 38, 224)',
+                   'rgba(3, 38, 224)',
+                   'rgba(3, 38, 224)',
+                   'rgba(3, 38, 224)',
+                   'rgba(3, 38, 224)',
+                   'rgba(3, 38, 224)',
+                   'rgba(3, 38, 224)',
+                //    'rgba(3, 38, 224)',
+               ],
+               borderColor: [
+                   'rgba(255, 99, 132, 0.2)',
+                   'rgba(255, 99, 132, 0.2)',
+                   'rgba(255, 99, 132, 0.2)',
+                   'rgba(255, 99, 132, 0.2)',
+                   'rgba(255, 99, 132, 0.2)',
+                   'rgba(255, 99, 132, 0.2)',
+                   'rgba(255, 99, 132, 0.2)',
+                   'rgba(255, 99, 132, 0.2)',
+                   'rgba(255, 99, 132, 0.2)',
+                   
+               ],
+
+               borderWidth: 1,
+        }
+       ]
+
+    }
+    const MTechSeatMatrix = {
+
+        labels: ["Civil Enginn.", "Computer Sci. Engin.", "Electrical Enginne.", "Mechanical Engin.","Electronics & comm. Enginne."  , "Architecture"],
+
+       datasets:[
+        {
+               labels: "",
+
+               data: [ 105, 40, 42, 57, 31, 19],
+
+               backgroundColor: [
+                   'rgba(3, 38, 224)',
+                   'rgba(3, 38, 224)',
+                   'rgba(3, 38, 224)',
+                   'rgba(3, 38, 224)',
+                   'rgba(3, 38, 224)',
+                   'rgba(3, 38, 224)',
+                //    'rgba(3, 38, 224)',
+                //    'rgba(3, 38, 224)',
+                //    'rgba(3, 38, 224)',
+               ],
+               borderColor: [
+                   'rgba(255, 99, 132, 0.2)',
+                   'rgba(255, 99, 132, 0.2)',
+                   'rgba(255, 99, 132, 0.2)',
+                   'rgba(255, 99, 132, 0.2)',
+                   'rgba(255, 99, 132, 0.2)',
+                   'rgba(255, 99, 132, 0.2)',
+                //    'rgba(255, 99, 132, 0.2)',
+                //    'rgba(255, 99, 132, 0.2)',
+                //    'rgba(255, 99, 132, 0.2)',
+                   
+               ],
+
+               borderWidth: 1,
+        }
+       ]
+
+    }
+    const BtechSeatMatriData = {
+
+        labels:["B Arch", "Civil Enginn." , "Computer Sci. Engin.","Electronics & comm. Enginne." , "Electrical Enginne." , "Mechanical Engin." , "Integrated Msc"],
+
+       datasets:[
+        {
+               labels: "",
+
+               data: [ 40,101, 157, 127, 100, 105, 50],
+
+               backgroundColor: [
+                   'rgba(3, 38, 224)',
+                   'rgba(3, 38, 224)',
+                   'rgba(3, 38, 224)',
+                   'rgba(3, 38, 224)',
+                   'rgba(3, 38, 224)',
+                   'rgba(3, 38, 224)',
+                   'rgba(3, 38, 224)',
+                   'rgba(3, 38, 224)',
+                //    'rgba(3, 38, 224)',
+               ],
+               borderColor: [
+                   'rgba(255, 99, 132, 0.2)',
+                   'rgba(255, 99, 132, 0.2)',
+                   'rgba(255, 99, 132, 0.2)',
+                   'rgba(255, 99, 132, 0.2)',
+                   'rgba(255, 99, 132, 0.2)',
+                   'rgba(255, 99, 132, 0.2)',
+                   'rgba(255, 99, 132, 0.2)',
+                   'rgba(255, 99, 132, 0.2)',
+                   'rgba(255, 99, 132, 0.2)',
+                   
+               ],
+
+               borderWidth: 1,
+        }
+       ]
+
+    }
+
+
+
+
+
       const data = {
-        labels: MONTHS,
+        labels: MONTHS,//y-axis
         datasets: [{
-          label: 'My First Dataset',
+          label: 'My First Dataset',//x-axis
           data: [65, 59, 80, 81, 56, 55, 40,0,20,30,25],
           backgroundColor: [
             'rgba(255, 99, 132, 0.2)',
@@ -62,27 +197,36 @@ function Statics() {
             'rgb(153, 102, 255)',
             'rgb(201, 203, 207)'
           ],
+        //   #5a6ed8
           borderWidth: 1
         }]
       };;
     return (
         <>
             <section className='py-20'>
+
+                <div>
+                   
+                </div>
                 <div className='text-center py-10'>
                     <h1 className='text-center text-5xl font-serif font-semibold text-red-900'>Seat Matrix</h1>
                 </div>
                 <div className='md:flex'>
-                    <div className='md:w-1/2'>
-                        <h1 className='text-center text-2xl font-medium'>B.tech Seat Matrix</h1>
-                        <div className="flex justify-center">
-                            <img src='https://tnp.nitp.ac.in/_next/image?url=%2F_next%2Fstatic%2Fimages%2FbTech_seatMatrix-84bdf4009b05ab43980b666181053838.png&w=640&q=75' className='' />
-                        </div>
+                    <div className='md:w-1/2 '>
+                        {/* <h1 className='text-center text-2xl font-medium'>B.tech Seat Matrix</h1> */}
+                        {/* <div className="flex justify-center h-full w-full "> */}
+                            <BarChart
+                                chartData={BtechSeatMatrixData}
+                                tittle={"B. Tech Seat Matrix"}
+                            />
+                        {/* </div> */}
                     </div>
                     <div className='md:w-1/2'>
-                        <h1 className='text-center text-2xl font-medium'>M.tech Seat Matrix</h1>
-                        <div className="flex justify-center">
-                            <img src="https://tnp.nitp.ac.in/_next/image?url=%2F_next%2Fstatic%2Fimages%2FmTech_seatMatrix-bfd6b54d92b21f9731696a2d875b6883.png&w=640&q=75" alt="" className='' />
-                        </div>
+                        {/* <h1 className='text-center text-2xl font-medium'>M.tech Seat Matrix</h1> */}
+                        <BarChart
+                            chartData={MTechSeatMatrix}
+                            tittle={"M. Tech Seat Matrix"}
+                        />
                     </div>
                 </div>
                 <div className='py-10'>
