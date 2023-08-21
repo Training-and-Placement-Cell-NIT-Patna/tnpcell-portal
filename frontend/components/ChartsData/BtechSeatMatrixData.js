e9ea08b893ee4a5eeeadf276ed72b03509995453
Sahitya Aryan <sahityaaryansingh@gmail.com>
2023-08-21 10:31:01 +0530

    Added : A file containing the Data of Btech seat Matrix

---
+++ frontend/components/ChartsData/BtechSeatMatrixData.js
@@ -0,0 +1,41 @@
+const BtechSeatMatriData = {
+
+    labels: ["B Arch", "Civil Enginn.", "Computer Sci. Engin.", "Electronics & comm. Enginne.", "Electrical Enginne.", "Mechanical Engin.", "Integrated Msc"],
+
+    datasets: [
+        {
+            labels:"",
+
+            data: [40, 101, 157, 127, 100, 105, 50],
+
+            backgroundColor: [
+                'rgba(3, 38, 224)',
+                'rgba(3, 38, 224)',
+                'rgba(3, 38, 224)',
+                'rgba(3, 38, 224)',
+                'rgba(3, 38, 224)',
+                'rgba(3, 38, 224)',
+                'rgba(3, 38, 224)',
+                'rgba(3, 38, 224)',
+                //    'rgba(3, 38, 224)',
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+
+            ],
+
+            borderWidth: 1,
+        }
+    ]
+
+}
+
+export default BtechSeatMatriData;
