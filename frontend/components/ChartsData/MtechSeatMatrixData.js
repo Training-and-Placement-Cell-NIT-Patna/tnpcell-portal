9b27bbfc75b1f744062a4623087a32492e031e7c
Sahitya Aryan <sahityaaryansingh@gmail.com>
2023-08-21 10:45:59 +0530

    Added : the data for Mtch Seat Matrix

---
+++ frontend/components/ChartsData/MtechSeatMatrixData.js
@@ -0,0 +1,34 @@
+const MTechSeatMatrix = {
+
+    labels: ["Civil Enginn.", "Computer Sci. Engin.", "Electrical Enginne.", "Mechanical Engin.", "Electronics & comm. Enginne.", "Architecture"],
+
+    datasets: [
+        {
+            labels: "",
+
+            data: [105, 40, 42, 57, 31, 19],
+
+            backgroundColor: [
+                'rgba(3, 38, 224)',
+                'rgba(3, 38, 224)',
+                'rgba(3, 38, 224)',
+                'rgba(3, 38, 224)',
+                'rgba(3, 38, 224)',
+                'rgba(3, 38, 224)',
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+
+            ],
+
+            borderWidth: 1,
+        }
+    ]
+
+}
+export default MTechSeatMatrix;
