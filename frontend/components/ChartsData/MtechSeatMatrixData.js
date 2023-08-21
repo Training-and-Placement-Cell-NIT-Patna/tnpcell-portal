1d6378d838e7d66d4a3bbcadb56b6a457e6e88d6
Sahitya Aryan <sahityaaryansingh@gmail.com>
2023-08-21 16:52:51 +0530

    Modified : modified the data of the Btech seat matrix so that it can render without using any functional components

--- frontend/components/ChartsData/MtechSeatMatrixData.js
+++ frontend/components/ChartsData/MtechSeatMatrixData.js
@@ -1,32 +1,17 @@
+const xLabel = ["Civil Enginn.", "Computer Sci. Engin.", "Electrical Enginne.", "Mechanical Engin.", "Electronics & comm. Enginne.", "Architecture"];
+const yLabelData = [105, 40, 42, 57, 31, 19];
 const MTechSeatMatrix = {
 
-    labels: ["Civil Enginn.", "Computer Sci. Engin.", "Electrical Enginne.", "Mechanical Engin.", "Electronics & comm. Enginne.", "Architecture"],
+    labels: xLabel,
 
     datasets: [
         {
-            labels: "",
-
-            data: [105, 40, 42, 57, 31, 19],
-
-            backgroundColor: [
-                'rgba(3, 38, 224)',
-                'rgba(3, 38, 224)',
-                'rgba(3, 38, 224)',
-                'rgba(3, 38, 224)',
-                'rgba(3, 38, 224)',
-                'rgba(3, 38, 224)',
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(255, 99, 132, 0.2)',
-
-            ],
-
-            borderWidth: 1,
+            type:'bar',
+            label: "No. of registered students",
+            data: yLabelData,
+            backgroundColor:"rgba(14,105,156,255)",
+            borderColor: "white",
+            borderWidth: 3,
         }
     ]
 
