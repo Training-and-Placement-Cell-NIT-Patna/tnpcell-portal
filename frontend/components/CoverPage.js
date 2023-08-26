a6ebb81e7f470c2525db7c34e598e65110146a44
Rajan kumar <krcpr007@gmail.com>
2023-08-26 16:47:13 +0530

    Navbar removed and created another component

--- frontend/components/CoverPage.js
+++ frontend/components/CoverPage.js
@@ -9,11 +9,12 @@ import Footer from "./LandingPageElements/Footer";
 import Process from "./LandingPageElements/Process";
 import WhyUS from "./LandingPageElements/WhyUS";
 import { Typewriter } from "react-simple-typewriter";
-import Image from "next/image";
+// import Image from "next/image";
 import Link from "next/link";
 import ProfessorIncharge from "./LandingPageElements/ProfessorIncharge";
 import Statics from "./LandingPageElements/Statics";
 import CompaniesMarquee from "./LandingPageElements/CompaniesMarquee";
+import CoverPageNavBar from "./CoverPageNavBar";
 
 const MENU_LIST = [
   {
@@ -85,105 +86,12 @@ function CoverPage() {
   };
   return (
     <>
-      <div className=" top-0 z-50">
-        <div className="">
-          <div className=" mx-auto z-50 bg-[#2c0707]  p-1 flex justify-between w-screen">
-            <div className="flex">
-              <Image
-                width={50}
-                height={50}
-                alt="NIT Patna"
-                src="/images/logo.png"
-                className="w-full"
-              />
-
-              <div className=" font-serif font-extrabold sm:text-2xl p-1 sm:p-2 flex items-center min-h-14 text-white ">
-                Training and Placement Cell
-              </div>
-            </div>
-
-            <div className="hidden lg:block h-10">
-              <div className="flex flex-row-reverse items-center my-2 mr-5">
-                {MENU_LIST.map((menu) => {
-                  return (
-                    <>
-                      <Link href={menu.href} key={menu.idx}>
-                        <p className="text-[#858383] hover:bg-[#67101075] cursor-pointer self-center p-3 rounded-md text-sm font-medium hover:text-yellow-200">
-                          {menu.text}
-                        </p>
-                      </Link>
-                    </>
-                  );
-                })}
-              </div>
-            </div>
-            <div className="flex lg:hidden ">
-              <button
-                onClick={() => setIsOpen(!isOpen)}
-                type="button"
-                className=" inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                aria-controls="mobile-menu"
-                aria-expanded="false"
-              >
-                <span className="sr-only">Open main menu</span>
-                {!isOpen ? (
-                  <svg
-                    className="block h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M4 6h16M4 12h16M4 18h16"
-                    />
-                  </svg>
-                ) : (
-                  <svg
-                    className="block h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M6 18L18 6M6 6l12 12"
-                    />
-                  </svg>
-                )}
-              </button>
-            </div>
-          </div>
-          {isOpen ? (
-            <>
-              <div className="lg:hidden relative h-10 z-50 bg-red-950">
-                <div className="">
-                  {MENU_LIST.map((menu) => (
-                    <Link href={menu.href} key={menu.idx}>
-                      <p className="text-[#858383] bg-red-950 self-center p-3 text-sm font-medium hover:text-yellow-200">
-                        {menu.text}
-                      </p>
-                    </Link>
-                  ))}
-                </div>
-              </div>
-            </>
-          ) : null}
-        </div>
-      </div>
+      <CoverPageNavBar />
       <main>
         {/* carousel  start */}
         <div ref={slideRef} className="w-full select-none">
           <div className="aspect-w-16 aspect-h-7">
-            <img src={carouselImage[currentIndex]} alt="" />
+            <img src={carouselImage[currentIndex]} alt="cover-images"/>
             <div className="absolute top-3/4 w-full">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 150">
                 <path
