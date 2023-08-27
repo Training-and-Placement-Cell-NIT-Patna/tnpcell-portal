b32efacd9f441902cd9eca9f2d587363dd3f3182
Rajan kumar <krcpr007@gmail.com>
2023-08-27 23:02:30 +0530

    Small change : Schema for coordinators is changed now

--- frontend/pages/contactus/index.js
+++ frontend/pages/contactus/index.js
@@ -47,7 +47,7 @@ function ContactUS() {
                   <div className="relative overflow-hidden transition duration-300 transform rounded shadow-lg lg:hover:-translate-y-2 hover:shadow-2xl">
                     <img
                       className="object-cover w-full h-64 md:h-64 xl:h-80"
-                      src={`${API_URL}${data.image.url}`}
+                      src={`${API_URL}${data?.image?.url}`}
                       alt="Person"
                     />
                     <div className="absolute inset-0 flex flex-col justify-center px-5 py-4 text-center transition-opacity duration-300 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
@@ -59,13 +59,13 @@ function ContactUS() {
                       </p>
                       <div className="flex items-center justify-center space-x-3">
                         <a
-                          href={data.social_media.email}
+                          href={data?.twitter}
                           className="text-white transition-colors duration-300 hover:text-teal-accent-400"
                         >
                           <BsTwitter />
                         </a>
                         <a
-                          href={data.social_media.linkedin}
+                          href={data?.linkedin}
                           className="text-white transition-colors duration-300 hover:text-teal-accent-400"
                         >
                           <BsLinkedin />
