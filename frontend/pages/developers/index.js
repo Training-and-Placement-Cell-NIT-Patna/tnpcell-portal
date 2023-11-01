1988f075d0ad481be426a791b48d3f8e35f49119
Sahitya Aryan <sahityaaryansingh@gmail.com>
2023-11-02 04:47:26 +0530

    Fix: fixed the allignment of the web team page at different screen size

--- frontend/pages/developers/index.js
+++ frontend/pages/developers/index.js
@@ -50,13 +50,13 @@ const Developers = () => {
         <title>Developers | T&P</title>
       </Head>
       <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-        <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
-          <div>
-            <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
+        <div className=" max-w-xl mb-10 mx-auto md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+          <div className={' flex justify-center'}>
+            <p className="inline-block px-3 py-px mb-4 text-md sm:text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
               Technical Team
             </p>
           </div>
-          <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
+          <h2 className=" max-w-lg mb-6 text-center font-sans text-2xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
             <span className="relative inline-block">
               <svg
                 viewBox="0 0 52 24"
@@ -84,7 +84,7 @@ const Developers = () => {
             </span>{" "}
             our talented team of professionals
           </h2>
-          <p className="text-base text-gray-700 md:text-lg">
+          <p className="text-base flex justify-center text-gray-700 md:text-lg">
             Training and Placement cell, NIT Patna
           </p>
         </div>
