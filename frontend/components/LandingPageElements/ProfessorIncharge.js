f99334f055aa170a4514aff9fadb36221938e66e
Sahitya Aryan <sahityaaryansingh@gmail.com>
2023-11-02 04:46:01 +0530

    Fix: corrected the spellling of the Dr. Samrat mukherjee

--- frontend/components/LandingPageElements/ProfessorIncharge.js
+++ frontend/components/LandingPageElements/ProfessorIncharge.js
@@ -16,7 +16,7 @@ function ProfessorIncharge() {
             <div className="flex justify-center">
               <img src="/images/Samart_mr.JPG" className="rounded-full w-1/2 md:w-2/5" />
             </div>
-            <h1 className="text-2xl">Dr. Samart Mukherjee</h1>
+            <h1 className="text-2xl">Dr. Samrat Mukherjee</h1>
             <h2>Prof. Incharge, T&P Cell NIT Patna</h2>
             {/*<p className="text-gray-500">+919334314098</p>*/}
             {/*<p className="text-gray-500">samart@nitp.ac.in</p>*/}
