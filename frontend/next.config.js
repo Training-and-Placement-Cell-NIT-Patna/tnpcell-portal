c3aa7a6402602cf16ec4ac11f0d1a1979ff0e519
Rajan kumar <krcpr007@gmail.com>
2023-11-04 23:14:19 +0530

    Fix : cors error

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,31 @@
 /** @type {import('next').NextConfig} */
+// https://stackoverflow.com/questions/70688660/cors-issue-next-js-application
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['localhost', '192.168.78.21','10.1.24.93'],
+    domains: ['localhost', '192.168.1.42', '14.139.219.168', 'tpc.nitp.ac.in','https://tpc.nitp.ac.in','http://tpc.nitp.ac.in'],
+  },
+  async headers() {
+    return [
+      {
+        source: "/api/(.*)",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" }
+        ]
+      }
+    ]
+  },
+  // @important
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*', // Define the path to match.
+        destination: 'http://192.168.1.42:1337/api/:path*', //
+      },
+    ]
   },
 }
 
