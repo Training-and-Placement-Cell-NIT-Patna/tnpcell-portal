5891fb4040a3b2afe3dd4c372aa520f3ddffbda3
Training and Placement Cell, NIT Patna <140609818+tnpcell-nitp@users.noreply.github.com>
2023-11-10 23:59:50 +0530

    IP change

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,6 +1,5 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '172.16.8.21'),
-  // 172.16.8.21
+  host: env('HOST', '192.168.1.42'),
   port: env.int('PORT', 1337),
   app: {
  keys: env.array('APP_KEYS'),
