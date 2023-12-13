fd8e63bce7aaa54ff980fda569bd6fb50d4581ba
Rajan kumar <krcpr007@gmail.com>
2023-12-14 01:52:10 +0530

    Feat : connecting prometheus

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -46,6 +46,40 @@ module.exports = ({ env }) => ({
   },
 
   // ... Other plugin configurations ...
+  'strapi-prometheus': {
+    enabled: true,
+    config: {
+      // follow -> https://market.strapi.io/plugins/strapi-prometheus
+      // add prefix to all the prometheus metrics names.
+      prefix: '',
+
+      // use full url instead of matched url
+      // true  => path label: `/api/models/1`
+      // false => path label: `/api/models/:id`
+      fullURL: false,
+
+      // include url query in the url label
+      // true  => path label: `/api/models?limit=1`
+      // false => path label: `/api/models`
+      includeQuery: false,
+
+      // metrics that will be enabled, by default they are all enabled.
+      enabledMetrics: {
+        koa: true, // koa metrics
+        process: true, // metrics regarding the running process
+        http: true, // http metrics like response time and size
+        apollo: true, // metrics regarding graphql
+      },
+
+      // interval at which rate metrics are collected in ms
+      interval: 1_000,
+
+      // set custom/default labels to all the prometheus metrics
+      customLabels: {
+        name: "strapi-prometheus",
+      },
+    }
+  }
 });
 
 
